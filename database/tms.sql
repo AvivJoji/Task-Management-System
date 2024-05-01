ef9d3f3d1ff92b41d293877c69a75999a2c10e50
AvivJoji <avivjoji@gmail.com>
2024-05-01 23:01:32 +0530

    first commit

---
+++ database/tms.sql
@@ -0,0 +1,1 @@
+SELECT * FROM tms_db.users;
---
+++ includes/connection.php
@@ -0,0 +1,4 @@
+<?php   
+    $connection = mysqli_connect("tms.cdwi86ee0phn.us-east-1.rds.amazonaws.com","admin","Taylorswift");
+    $db = mysqli_select_db($connection,"tms_db");
+?>
